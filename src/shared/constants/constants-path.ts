73bbff9b7712a081616a0ef135f532cf69c36e74
Mathilde <mathilde@forssea-robotics.fr>
2023-12-05 15:00:12 +0100

    archi: add constants path

---
+++ src/shared/constants/constants-path.ts
@@ -0,0 +1,15 @@
+/**
+ * This file contains path constants
+ */
+
+
+// ---------------------
+// ######## API RELATIVE
+// ---------------------
+
+// --- THRUSTER
+export const API_PATH_ENGINE_LIST = '/rocket/engine_list';
+export const API_PATH_ENGINE = '/rocket/engine';
+
+// --- RADIO
+export const API_PATH_RADIO = '/rocket/tools/radio';

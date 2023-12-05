70df164ca612175ba9aac55d968bc8b119871aa6
Mathilde <mathilde@forssea-robotics.fr>
2023-12-05 17:34:28 +0100

    API: add a base path

--- src/shared/constants/constants-path.ts
+++ src/shared/constants/constants-path.ts
@@ -7,9 +7,11 @@
 // ######## API RELATIVE
 // ---------------------
 
+const BASE_PATH = '/api/rocket';
+
 // --- THRUSTER
-export const API_PATH_ENGINE_LIST = '/rocket/engine_list';
-export const API_PATH_ENGINE = '/rocket/engine';
+export const API_PATH_ENGINE_LIST = `${BASE_PATH}/engine_list`;
+export const API_PATH_ENGINE = `${BASE_PATH}/engine`;
 
 // --- RADIO
-export const API_PATH_RADIO = '/rocket/tools/radio';
+export const API_PATH_RADIO = `${BASE_PATH}/tools/radio`;

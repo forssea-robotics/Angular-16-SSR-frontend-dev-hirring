6bd0aa59fe8d41390d5c3352712189f89688e501
Mathilde <mathilde@forssea-robotics.fr>
2023-12-07 10:42:22 +0100

    project-name: change prokect name to Rocketx

--- src/frontend/app/app.component.spec.ts
+++ src/frontend/app/app.component.spec.ts
@@ -12,16 +12,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'angular-ssr-starter'`, () => {
+  it(`should have as title 'rocketx-hmi'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('angular-ssr-starter');
+    expect(app.title).toEqual('rocketx-hmi');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('.content span')?.textContent).toContain('angular-ssr-starter app is running!');
+    expect(compiled.querySelector('.content span')?.textContent).toContain('rocketx-hmi app is running!');
   });
 });

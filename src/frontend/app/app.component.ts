59257dab991342b1d9cf8a70aa767e9bbb61d19e
Pierre Sachot <pierre@forssea-robotics.fr>
2023-12-07 16:01:35 +0100

    feat: add datetime widget in top of the navbar

--- src/frontend/app/app.component.ts
+++ src/frontend/app/app.component.ts
@@ -1,4 +1,7 @@
-import { Component } from '@angular/core';
+/* eslint-disable @typescript-eslint/ban-types */
+import { formatDate, isPlatformServer } from '@angular/common';
+import { Component, Inject, PLATFORM_ID } from '@angular/core';
+import { map, timer } from 'rxjs';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +9,32 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'rocketx-hmi';
+  public title = 'rocketx-hmi';
+  public browserTimeStr?: string;
+  public browserDateStr?: string;
+
+  constructor(
+    @Inject(PLATFORM_ID) private _platformId: Object
+  ) {
+    if(isPlatformServer(this._platformId)) return; // As we are using SSR, we don't want to run this code on the server
+
+    // Get the current browser (a.k.a system) time every seconds
+    timer(0, 1000)
+      .pipe(
+        map(() => new Date())
+      ).subscribe(this._generateDateStr.bind(this));
+  }
+
+  private _generateDateStr(date: Date): void {
+    this.browserTimeStr = this._dateToHourMinutesSeconds(date);
+    this.browserDateStr = this._dateToYearMonthDay(date);
+  }
+
+  private _dateToHourMinutesSeconds(date: Date): string {
+    return formatDate(date, 'hh:mm:ss a', 'en-EN');
+  }
+
+  private _dateToYearMonthDay(date: Date): string {
+    return formatDate(date, 'YYYY-MM-dd', 'en-EN');
+  }
 }
